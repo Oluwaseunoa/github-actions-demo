1811cf83bd2f6ab91a48f46b1e0e0695a382aacd
Oluwaseun Osunsola <oluwaseun.beicks@gmail.com>
2026-01-17 13:58:48 +0100

    feat: enable automated semantic releases

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! CI Pipeline Active 🚀');
+  res.send('Hello, World! Automated Releases Active 🚀');
 });
 
 app.listen(port, () => {
