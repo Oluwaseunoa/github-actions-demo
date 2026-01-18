467d8ac9b30444fba6acae58fd7ae1cb3c34ae8f
Oluwaseun Osunsola <oluwaseun.beicks@gmail.com>
2026-01-18 02:47:14 +0100

    feat: trigger production deployment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! Automated Releases Active 🚀');
+  res.send('Hello, World! Deployed automatically to AWS 🚀');
 });
 
 app.listen(port, () => {
